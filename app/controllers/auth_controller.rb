7331c3634a5e454d2ccce34ea686fd0703d4bd92
rasheed <thcmofskot@gmail.com>
2024-03-19 10:38:44 +0100

    register controller; functional

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,18 @@
 class AuthController < ApplicationController
+  skip_forgery_protection
+
   def register
+    if request.post?
+      user = User.new
+      user.username = params[:username]
+      user.password = params[:password]
+      if user.save
+        flash[:notice] = "You have successfully registered"
+        redirect_to :action => "login"
+      else
+        flash[:error] = "There was a problem registering"
+      end
+    end
   end
 
   def login
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get "home/index"
+
   get "auth/register"
+  post "auth/register"
+
   get "auth/login"
+  post "auth/login"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
