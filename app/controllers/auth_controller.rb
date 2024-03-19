894af7db405776f6a13ef3933762496392228272
rasheed <thcmofskot@gmail.com>
2024-03-19 11:35:37 +0100

    comments auth_controller and adds error handling

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,21 +1,33 @@
+# The AuthController handles user authentication and registration.
 class AuthController < ApplicationController
   skip_forgery_protection
 
+  # Registers a new user.
   def register
     if request.post?
       @user = User.new(
         params[:user]
       )
       @user.password = params[:password]
-      if @user.save!
+      if @user.save
         flash[:notice] = "You have successfully registered"
         redirect_to :action => "login"
       else
         flash[:error] = "There was a problem registering"
       end
     end
+  rescue StandardError => e
+    flash[:error] = "An error occurred: #{e.message}"
+    redirect_to :action => "login"
   end
 
+  # Logs in a user.
   def login
+    if request.post?
+      @user = User.find_by(username: params[:username])&.authenticate(params[:password])
+    end
+  rescue StandardError => e
+    flash[:error] = "An error occurred: #{e.message}"
+    redirect_to :action => "login"
   end
 end
