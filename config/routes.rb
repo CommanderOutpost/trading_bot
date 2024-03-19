4d60484b86c6b5064e1f67b59753d233f2098168
rasheed <thcmofskot@gmail.com>
2024-03-19 09:45:18 +0100

    adds landing page along with application.html and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root "home#index"
   get "auth/register"
   get "auth/login"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
