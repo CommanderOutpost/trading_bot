3bae31d0cf14e58b6b76cc03121e1f38a88458cb
rasheed <thcmofskot@gmail.com>
2024-04-22 15:21:28 +0100

    adds localhost ot allowed hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  config.hosts = ["trading-bot-rkbt.onrender.com", "127.0.0.1:3000", "trading-bot-1-test.onrender.com", "0.0.0.0:3000", "trading_bot-1-test.onrender.com"]
+  config.hosts = ["trading-bot-rkbt.onrender.com", "127.0.0.1:3000", "trading-bot-1-test.onrender.com", "0.0.0.0:3000", "trading_bot-1-test.onrender.com", "localhost:3000"]
 end
