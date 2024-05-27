b27ea24533e20074ca479e6672c251a4860cd5e6
rasheed <thcmofskot@gmail.com>
2024-05-27 14:52:29 +0100

    adds password view and check to change password page

--- app/javascript/checkRegisterPassword.js
+++ app/javascript/checkRegisterPassword.js
@@ -1,12 +1,28 @@
-document.getElementById('signupForm').addEventListener('submit', function(event) {
-    var password = document.getElementById('password').value;
+var form = document.getElementById('signupForm');
+
+if (!form) {
+    form = document.getElementById('changePasswordForm');
+}
+
+form.addEventListener('submit', function (event) {
+    var password = document.getElementById('password');
+
+    if (!password) {
+        var password = document.getElementById('current_password');
+    }
+
+    const passwordValue = password.value;
+
     var passwordHelpBlock = document.getElementById('passwordHelpBlock');
+    passwordHelpBlock.style.marginBottom = "5px";
 
     // Regular expression to match the password requirements
     var passwordPattern = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,20}$/;
 
+    console.log(passwordPattern.test(passwordValue))
 
-    if (!passwordPattern.test(password)) {
+
+    if (!passwordPattern.test(passwordValue)) {
         event.preventDefault();
         passwordHelpBlock.textContent = "Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.";
         passwordHelpBlock.style.color = 'red';
--- app/javascript/viewPassword.js
+++ app/javascript/viewPassword.js
@@ -1,6 +1,10 @@
-document.getElementById('togglePassword').addEventListener('click', function (e) {
-    const passwordInput = document.getElementById('password');
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-    this.querySelector('i').className = type === 'password' ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye';
+const togglePasswordButtons = document.querySelectorAll('.togglePassword');
+
+togglePasswordButtons.forEach((toggleButton) => {
+    toggleButton.addEventListener('click', function () {
+        const passwordInput = this.previousElementSibling;
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+        this.querySelector('i').className = type === 'password' ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye';
+    });
 });
